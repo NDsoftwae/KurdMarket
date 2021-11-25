8d3f8d9c8380fb1c4e80a16fd97603f6e310e008
Nzar <nzar.r.ahmed@gmail.com>
2021-11-25 00:30:51 +0000

    Deploy the Site 24 NOV

---
+++ Controllers/Test.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KurdMarket.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get() => Ok("This service is running through CI/CD from github to Azure machine....");
+    }
+}
